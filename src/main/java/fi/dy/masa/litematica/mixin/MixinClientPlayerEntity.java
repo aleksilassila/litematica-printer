cbbea9ce909bd75bfd017ccdac3c67834e3efd90
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2020-10-06 20:28:58 +0300

    Slabs now placed correctly

--- src/main/java/fi/dy/masa/litematica/mixin/MixinClientPlayerEntity.java
+++ src/main/java/fi/dy/masa/litematica/mixin/MixinClientPlayerEntity.java
@@ -5,6 +5,7 @@ import fi.dy.masa.litematica.config.Configs;
 import fi.dy.masa.litematica.interfaces.IClientPlayerInteractionManager;
 import fi.dy.masa.litematica.world.SchematicWorldHandler;
 import fi.dy.masa.litematica.world.WorldSchematic;
+import fi.dy.masa.malilib.util.BlockUtils;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
@@ -15,6 +16,9 @@ import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.MovementType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
@@ -27,6 +31,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 @Mixin(ClientPlayerEntity.class)
 public class MixinClientPlayerEntity extends AbstractClientPlayerEntity {
@@ -68,7 +74,7 @@ public class MixinClientPlayerEntity extends AbstractClientPlayerEntity {
     				// Check if player is holding right block
     				if (!isBlockInHand(targetSchematicBlock)) continue;
 
-					if (tryToPlace(pos)) {
+					if (tryToPlaceBlock(pos)) {
 						lastPlaced = new Date().getTime();
 						break loop;
 					};
@@ -77,16 +83,51 @@ public class MixinClientPlayerEntity extends AbstractClientPlayerEntity {
 		}
     }
 
-    private boolean tryToPlace(BlockPos pos) {
+    int getBlockHalf(BlockState state) {
+    	for (Property<?> prop : state.getProperties()) {
+			if (prop.getName().equals("type") || prop.getName().equals("half")) {
+				return state.get(prop).toString().equals("top") ? 1 : 0;
+			}
+		}
+
+    	return -1;
+	}
+
+	Direction getBlockDirection(BlockState state) {
+    	for (Property<?> prop : state.getProperties()) {
+			if (prop instanceof DirectionProperty) {
+				return (Direction)state.get(prop);
+			}
+		}
+
+    	return null;
+	}
+
+    private boolean tryToPlaceBlock(BlockPos pos) {
+    	BlockState state = SchematicWorldHandler.getSchematicWorld().getBlockState(pos);
+    	Direction dir = BlockUtils.getFirstPropertyFacingValue(state);
+
 		Vec3d posVec = Vec3d.ofCenter(pos);
 
+		Direction schDir = getBlockDirection(state);
+		int half = getBlockHalf(state);
+
 		for(Direction side : Direction.values()) {
+			if (half == 1 && side.equals(Direction.DOWN)) continue;
+			if (half == 0 && side.equals(Direction.UP)) continue;
+
 			BlockPos neighbor = pos.offset(side);
 
 			if (!canBeClicked(neighbor))
 				continue;
 
 			Vec3d hitVec = posVec.add(Vec3d.of(side.getVector()).multiply(0.5));
+
+			if (half == 1 && !side.equals(Direction.UP)) {
+				hitVec = hitVec.add(0, 0.25, 0);
+			} else if (half == 0 && !side.equals(Direction.DOWN)) {
+				hitVec = hitVec.add(0, -0.25, 0);
+			}
 
 			((IClientPlayerInteractionManager) client.interactionManager).rightClickBlock(neighbor,
 					side.getOpposite(), hitVec);
