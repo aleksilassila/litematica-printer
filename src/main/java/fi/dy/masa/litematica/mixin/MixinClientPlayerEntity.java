e2f1c9223da15f87c3af89b357a18d993284a376
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2021-01-06 17:52:45 +0200

    Added update checker, fixed printing in layered mode

--- src/main/java/fi/dy/masa/litematica/mixin/MixinClientPlayerEntity.java
+++ src/main/java/fi/dy/masa/litematica/mixin/MixinClientPlayerEntity.java
@@ -3,11 +3,14 @@ package fi.dy.masa.litematica.mixin;
 import com.mojang.authlib.GameProfile;
 import fi.dy.masa.litematica.config.Configs;
 import fi.dy.masa.litematica.printer.Printer;
+import fi.dy.masa.litematica.printer.UpdateChecker;
 import fi.dy.masa.litematica.world.SchematicWorldHandler;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.network.MessageType;
+import net.minecraft.text.LiteralText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,6 +22,7 @@ import java.util.Date;
 
 @Mixin(ClientPlayerEntity.class)
 public class MixinClientPlayerEntity extends AbstractClientPlayerEntity {
+	boolean didCheckForUpdates = false;
 
     public MixinClientPlayerEntity(ClientWorld world, GameProfile profile) {
         super(world, profile);
@@ -38,6 +42,12 @@ public class MixinClientPlayerEntity extends AbstractClientPlayerEntity {
 
 	@Inject(at = @At("HEAD"), method = "tick")
 	public void tick(CallbackInfo ci) {
+		if (!didCheckForUpdates) {
+			didCheckForUpdates = true;
+
+			checkForUpdates();
+		}
+
 		if (printer == null) {
 			if (client != null && client.player != null && client.world != null) {
 				printer = new Printer(client, client.player, client.world);
@@ -49,5 +59,18 @@ public class MixinClientPlayerEntity extends AbstractClientPlayerEntity {
 		if (SchematicWorldHandler.getSchematicWorld() == null || !Configs.Generic.PRINT_MODE.getBooleanValue()) return;
 
 		printer.print();
+	}
+
+	public void checkForUpdates() {
+        new Thread(() -> {
+            String version = UpdateChecker.version;
+            String newVersion = UpdateChecker.getPrinterVersion();
+
+            if (!version.equals(newVersion)) {
+                client.inGameHud.addChatMessage(MessageType.SYSTEM,
+                        new LiteralText("New version of Litematica Printer available in https://github.com/aleksilassila/litematica-printer/releases"),
+                        null);
+            }
+        }).start();
 	}
 }
