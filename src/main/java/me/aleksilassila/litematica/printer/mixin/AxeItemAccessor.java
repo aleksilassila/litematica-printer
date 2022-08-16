0ab6fbf10057d5953654fa862a1cad7152dddf25
byquanton <32410361+byquanton@users.noreply.github.com>
2022-08-16 22:29:01 +0200

    Create own AxeItemAccessor (Makes Mod compatible with Quilt and other Fabric API Implementations)

---
+++ src/main/java/me/aleksilassila/litematica/printer/mixin/AxeItemAccessor.java
@@ -0,0 +1,17 @@
+package me.aleksilassila.litematica.printer.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.AxeItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@Mixin(AxeItem.class)
+public interface AxeItemAccessor {
+    @Accessor("STRIPPED_BLOCKS")
+    static Map<Block, Block> getStrippedBlocks() {
+        throw new AssertionError("Untransformed @Accessor");
+    }
+
+}
