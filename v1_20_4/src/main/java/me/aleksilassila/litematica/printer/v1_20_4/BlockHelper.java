7311a2f8ab7a7ca326dce2ec61a5178a9a4e9025
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2023-12-23 02:44:16 +0200

    Temporary release for 1.20.4

---
+++ v1_20_4/src/main/java/me/aleksilassila/litematica/printer/v1_20_4/BlockHelper.java
@@ -0,0 +1,31 @@
+package me.aleksilassila.litematica.printer.v1_20_4;
+
+import net.minecraft.block.*;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+abstract public class BlockHelper {
+    public static List<Class<?>> interactiveBlocks = new ArrayList<>(Arrays.asList(
+            AbstractChestBlock.class, AbstractFurnaceBlock.class, CraftingTableBlock.class, LeverBlock.class,
+            DoorBlock.class, TrapdoorBlock.class, BedBlock.class, RedstoneWireBlock.class, ScaffoldingBlock.class,
+            HopperBlock.class, EnchantingTableBlock.class, NoteBlock.class, JukeboxBlock.class, CakeBlock.class,
+            FenceGateBlock.class, BrewingStandBlock.class, DragonEggBlock.class, CommandBlock.class,
+            BeaconBlock.class, AnvilBlock.class, ComparatorBlock.class, RepeaterBlock.class,
+            DropperBlock.class, DispenserBlock.class, ShulkerBoxBlock.class, LecternBlock.class,
+            FlowerPotBlock.class, BarrelBlock.class, BellBlock.class, SmithingTableBlock.class,
+            LoomBlock.class, CartographyTableBlock.class, GrindstoneBlock.class,
+            StonecutterBlock.class, AbstractSignBlock.class, AbstractCandleBlock.class));
+
+    public static final Item[] SHOVEL_ITEMS = new Item[]{
+            Items.NETHERITE_SHOVEL,
+            Items.DIAMOND_SHOVEL,
+            Items.GOLDEN_SHOVEL,
+            Items.IRON_SHOVEL,
+            Items.STONE_SHOVEL,
+            Items.WOODEN_SHOVEL
+    };
+}
---
+++ v1_20_4/src/main/java/me/aleksilassila/litematica/printer/v1_20_4/mixin/ConfigsMixin.java
@@ -0,0 +1,35 @@
+package me.aleksilassila.litematica.printer.v1_20_4.mixin;
+
+import com.google.common.collect.ImmutableList;
+import fi.dy.masa.litematica.config.Configs;
+import fi.dy.masa.malilib.config.IConfigBase;
+import fi.dy.masa.malilib.config.options.ConfigHotkey;
+import me.aleksilassila.litematica.printer.v1_20_4.LitematicaMixinMod;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.List;
+
+@Mixin(value = Configs.class, remap = false)
+public class ConfigsMixin {
+    @Redirect(method = "loadFromFile", at = @At(value = "FIELD", target = "Lfi/dy/masa/litematica/config/Configs$Generic;OPTIONS:Lcom/google/common/collect/ImmutableList;"))
+    private static ImmutableList<IConfigBase> moreOptions() {
+        return LitematicaMixinMod.getConfigList();
+    }
+
+    @Redirect(method = "saveToFile", at = @At(value = "FIELD", target = "Lfi/dy/masa/litematica/config/Configs$Generic;OPTIONS:Lcom/google/common/collect/ImmutableList;"))
+    private static ImmutableList<IConfigBase> moreeOptions() {
+        return LitematicaMixinMod.getConfigList();
+    }
+
+    @Redirect(method = "loadFromFile", at = @At(value = "FIELD", target = "Lfi/dy/masa/litematica/config/Hotkeys;HOTKEY_LIST:Ljava/util/List;"))
+    private static List<ConfigHotkey> moreHotkeys() {
+        return LitematicaMixinMod.getHotkeyList();
+    }
+
+    @Redirect(method = "saveToFile", at = @At(value = "FIELD", target = "Lfi/dy/masa/litematica/config/Hotkeys;HOTKEY_LIST:Ljava/util/List;"))
+    private static List<ConfigHotkey> moreeHotkeys() {
+        return LitematicaMixinMod.getHotkeyList();
+    }
+}
